a3cd7b7d3193e7799396d0ef399347bdc198a365
DEZMOND-ART <vlad.blender3d@gmail.com>
2024-10-02 18:13:21 +0300

    Update modul_2_2_if_elif_else.py

--- Modul_2/modul_2_2_if_elif_else.py
+++ Modul_2/modul_2_2_if_elif_else.py
@@ -3,7 +3,7 @@ second = (input('Ведите второе число: '))
 third = (input('Ведите третье число: '))
 if first == second and second == third:
     print('3')
-elif first == second or second == third:
+elif first == second or second == third or first == third:
     print('2')
 else:
     print('0')
