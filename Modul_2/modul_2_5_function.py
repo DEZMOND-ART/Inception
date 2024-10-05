f2ab747385a281a294be9f5af183e380e2936f76
vlad <vlad.blender3d@gmail.com>
2024-10-05 22:14:13 +0300

    05/10/2024

---
+++ Modul_2/modul_2_5_function.py
@@ -0,0 +1,18 @@
+def get_matrix(n, m, value):
+    matrix = []
+    for _ in range(n):
+        line = []
+        matrix.append(line)
+        for _ in range(m):
+            line.append(value)
+    return matrix
+
+
+# Тест
+result1 = get_matrix(1, 1, 1)
+result2 = get_matrix(2, 2, 2)
+result3 = get_matrix(3, 3, 3)
+
+print(result1)
+print(result2)
+print(result3)
