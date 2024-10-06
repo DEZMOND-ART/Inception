e80c8c93d5da07effc0e477237cffd58283bc689
vlad <vlad.blender3d@gmail.com>
2024-10-07 00:25:40 +0300

    07/10/2024

---
+++ Modul_2/modul_2_hard.py
@@ -0,0 +1,14 @@
+import random
+
+
+def stone():
+    n = random.choice(range(3,21))
+    print(f'число на первом камне: {n}')
+    result = []
+    for a in range(1,21):
+        for b in range(a + 1, 21):
+            if n % (a + b) == 0:
+                result.append(str(a))
+                result.append(str(b))
+    print(f'{n} - {"".join(result)}')
+stone()
