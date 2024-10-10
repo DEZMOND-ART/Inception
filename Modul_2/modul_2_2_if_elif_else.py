58fdbf81f84e39c8743fb863c11d98630d070362
vlad <vlad.blender3d@gmail.com>
2024-10-10 15:53:09 +0300

    optimized code

--- Modul_2/modul_2_2_if_elif_else.py
+++ Modul_2/modul_2_2_if_elif_else.py
@@ -1,6 +1,6 @@
-first = input('Ведите значение для сравнения: ')
-second = input('Ведите значение для сравнения: ')
-third = input('Ведите значение для сравнения: ')
+first = input('Введите значение для сравнения: ')
+second = input('Введите значение для сравнения: ')
+third = input('Введите значение для сравнения: ')
 if first == second and second == third:
     print('3')
 elif first == second or second == third or first == third:
