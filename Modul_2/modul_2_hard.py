4b7b390a51758fceee5ba68b40cce0369655d82e
vlad <vlad.blender3d@gmail.com>
2024-10-10 15:37:11 +0300

    optimized code

--- Modul_2/modul_2_hard.py
+++ Modul_2/modul_2_hard.py
@@ -2,13 +2,14 @@ import random
 
 
 def stone():
-    n = random.choice(range(3,21))
-    print(f'число на первом камне: {n}')
+    n = random.choice(range(3, 21))
     result = []
-    for a in range(1,21):
-        for b in range(a + 1, 21):
+    for a in range(1, n // 2 + 1):
+        for b in range(a + 1, n - a + 1):
             if n % (a + b) == 0:
-                result.append(str(a))
-                result.append(str(b))
+                result += str(a) + str(b)
+    print(f'число на первом камне: {n}')
     print(f'{n} - {"".join(result)}')
-    stone()
+
+
+stone()
