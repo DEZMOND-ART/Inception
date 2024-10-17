318cd50ffb2000d8901913d956e4baf2ab590ac4
vlad <vlad.blender3d@gmail.com>
2024-10-17 14:39:45 +0300

    Fix

--- Modul_3/modul_3_4_star_random.py
+++ Modul_3/modul_3_4_star_random.py
@@ -1,11 +1,12 @@
 def single_root_words(root_word, *other_words):
-    root_word = root_word.lower()
-    same_words = []
-    for word in other_words:
-        word_lower = word.lower()
-        if root_word in word_lower or word_lower in root_word:
-            same_words.append(word)
-    return same_words
+    return [word for word in other_words if root_word.lower() in word.lower() or word.lower() in root_word.lower()]
+    # root_word = root_word.lower()
+    # same_words = []
+    # for word in other_words:
+    #     word_lower = word.lower()
+    #     if root_word in word_lower or word_lower in root_word:
+    #         same_words.append(word)
+    # return same_words
 
 
 # TEST
