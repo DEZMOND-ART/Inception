09d482e1a8cb9247a2963283a42dcdbbf39594df
vlad <vlad.blender3d@gmail.com>
2024-10-17 13:43:16 +0300

    Fix

--- Modul_3/modul_3_2_calling_a_function.py
+++ Modul_3/modul_3_2_calling_a_function.py
@@ -1,10 +1,9 @@
-def send_email(message, recipient, sender='university.help@gmail.com'):
-    Domain_Group = ['.com', '.ru', '.net']
-# Если "@" нет в recipient или нет any - последовательная проверка на хотябы 1 совподение endswith проверка окончания строки
-    if '@' not in recipient or not any(recipient.endswith(domain) for domain in Domain_Group):
+def send_email(message, recipient, *, sender='university.help@gmail.com'):
+    # Если "@" нет в recipient или нет,endswith проверка окончания строки
+    if '@' not in recipient or not recipient.endswith(('.com', '.ru', '.net')):
         print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
         return
-    if '@' not in sender or not any(sender.endswith(domain) for domain in Domain_Group):
+    if '@' not in sender or not sender.endswith(('.com', '.ru', '.net')):
         print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
         return
     if sender == recipient:
@@ -18,6 +17,7 @@ def send_email(message, recipient, sender='university.help@gmail.com'):
 
 # Тест
 send_email('Это сообщение для проверки связи', 'Im@mail.ru')
-send_email('Вы видите это сообщение как лучший студент Hogwarts!', 'Hogwarts@gmail.com', sender='Hogwarts.info@gmail.com')
+send_email('Вы видите это сообщение как лучший студент Hogwarts!', 'Hogwarts@gmail.com',
+           sender='Hogwarts.info@gmail.com')
 send_email('Пожалуйста, исправьте задание', 'Hogwarts.student@gmail.com', sender='university.help@gmail.int')
 send_email('Напоминаю самому себе о вебинаре', 'Hogwarts.teacher@gmail.com', sender='Hogwarts.teacher@gmail.com')
