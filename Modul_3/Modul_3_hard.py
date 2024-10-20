cd328c737fba9bb96402e1c42dd8821e6b6359f9
vlad <vlad.blender3d@gmail.com>
2024-10-20 11:51:22 +0300

    20/10/2024

---
+++ Modul_3/Modul_3_hard.py
@@ -0,0 +1,30 @@
+def calculate_structure_sum(*args):
+    result = 0
+
+    # Проверяет элементы рекурсивно - грубо говоря открывает капот
+    for i in args:
+        if isinstance(i, (list, tuple, set)):  # Если i - список, кортеж или множество
+            result += calculate_structure_sum(*i)  # Достает i из data_structure и + к result
+        elif isinstance(i, dict):  # Если i - словарь
+            result += calculate_structure_sum(*i.keys())  # + ключи
+            result += calculate_structure_sum(*i.values())  # + значения
+        elif isinstance(i, (int, float)):  # Если i - число
+            result += i  # + число
+        elif isinstance(i, str):  # Если i - строка
+            # сложить(Превратить j в int / если j может быть int / иначе считать цифрой 1
+            # подставить в j последовательно все из i)
+            result += sum(int(j) if j.isdigit() else 1 for j in i)  # Сумма цифр и 1 за символ
+    return result
+
+
+# Пример тестирования
+data_structure = [
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+
+test = calculate_structure_sum(*data_structure)
+print(f"Сумма чисел и символов: {test}")
