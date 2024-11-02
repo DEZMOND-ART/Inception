ba0dbb441b01ada2b197b630084a033b3812fa39
vlad <vlad.blender3d@gmail.com>
2024-11-02 19:56:11 +0300

    fix

--- Modul_5/modul_5_2__len__str__(self).py
+++ Modul_5/modul_5_2__len__str__(self).py
@@ -17,7 +17,7 @@ class House:
 
 
     def __str__(self):
-        return (f'Название:{self.name}, кол-во этажей:{self.number_of_floors}')
+        return f'Название:{self.name}, кол-во этажей:{self.number_of_floors}'
 
 
 separator = '-' * 305
