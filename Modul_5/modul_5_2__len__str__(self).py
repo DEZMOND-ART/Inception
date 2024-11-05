f3bb4b81ad402497685ce7d6c3d8dd82d7d17bdd
vlad <vlad.blender3d@gmail.com>
2024-11-05 16:57:49 +0300

    Fix

--- Modul_5/modul_5_2__len__str__(self).py
+++ Modul_5/modul_5_2__len__str__(self).py
@@ -4,11 +4,11 @@ class House:
         self.number_of_floors = number_of_floors
 
     def go_to(self, new_floor):
-            if 1 <= new_floor <= self.number_of_floors:
-                for i in range(1, new_floor + 1):
-                    print(f"Этаж:{i}")
-            else:
-                print("Такого этажа не существует")
+        if 1 <= new_floor <= self.number_of_floors:
+            for i in range(1, new_floor + 1):
+                print(f"Этаж:{i}")
+        else:
+            print("Такого этажа не существует")
 
     def __len__(self):
         return self.number_of_floors
@@ -32,3 +32,4 @@ print(h2)
 # __len__
 print(len(h1))
 print(len(h2))
+
