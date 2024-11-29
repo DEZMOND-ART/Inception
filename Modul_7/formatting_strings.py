1d1dcbd7988b6bb2dce58e6fb84eb9b1eb82360e
vlad <vlad.blender3d@gmail.com>
2024-11-29 09:34:30 +0300

    29/11/24

--- Modul_7/formatting_strings.py
+++ Modul_7/formatting_strings.py
@@ -1,39 +1,25 @@
-# Входные данные
-team1_num = 5
-team2_num = 6
-score_1 = 40
-score_2 = 42
-team1_time = 1552.512
-team2_time = 2153.31451
-tasks_total = 82
-time_avg = 45.2
+import os
+import time
 
-# Логика для определения результата
-if score_1 > score_2 or (score_1 == score_2 and team1_time > team2_time):
-    challenge_result = "Победа команды Мастера кода!"
-elif score_1 < score_2 or (score_1 == score_2 and team1_time < team2_time):
-    challenge_result = "Победа команды Волшебники данных!"
-else:
-    challenge_result = "Ничья!"
+# Каталог для сканирования
+directory = "."
 
-# Использование % для форматирования строк
-result1 = "В команде Мастера кода участников: %d !" % team1_num
-result2 = "Итого сегодня в командах участников: %d и %d !" % (team1_num, team2_num)
+# Сканирование
+for root, dirs, files in os.walk(directory):
+    for file in files:
+        # Полный путь к файлу
+        filepath = os.path.join(root, file)
 
-# Использование format() для форматирования строк
-result3 = "Команда Волшебники данных решила задач: {} !".format(score_2)
-result4 = "Волшебники данных решили задачи за {:.1f} с !".format(team1_time)
+        # Время последнего изменения файла
+        filetime = os.path.getmtime(filepath)
+        formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
 
-# Использование f-строк для форматирования строк
-result5 = f"Команды решили {score_1} и {score_2} задач."
-result6 = f"Результат битвы: {challenge_result}"
-result7 = f"Сегодня было решено {tasks_total} задач, в среднем по {time_avg:.1f} секунды на задачу!"
+        # Размер файла
+        filesize = os.path.getsize(filepath)
 
-# Вывод всех результатов
-print(result1)
-print(result2)
-print(result3)
-print(result4)
-print(result5)
-print(result6)
-print(result7)
+        # Родительская директория файла
+        parent_dir = os.path.dirname(filepath)
+
+        # Информация о файле
+        print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, '
+              f'Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
