4d0f88f933fa5d922936bb26fc93afe6628949a7
vlad <vlad.blender3d@gmail.com>
2024-12-11 15:08:46 +0300

    Fix

--- Modul_9/modul_9_2.py
+++ Modul_9/modul_9_2.py
@@ -4,7 +4,7 @@ second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler
 # >=5
 first_result = [len(s) for s in first_strings if len(s) >= 5]
 # Список кортежей из слов одинаковой длинны
-second_result = [(f, s) for f in second_strings for s in second_strings if len(f) == len(s)]
+second_result = [(f, s) for f in first_strings for s in second_strings if len(f) == len(s)]
 # Словарь, где ключ - строка, а значение - длинна слова. Только четные
 third_result = {s: len(s) for s in first_strings + second_strings if len(s) % 2 == 0}
 
